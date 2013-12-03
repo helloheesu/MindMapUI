76985345d8bcd3cd39dc11e730831fed9cdb18bc
Heesu <heesu@nhnnext.org>
2013-12-04 02:48:56 +0900

    code refactoring : no using global variation, at least in function

--- div.js
+++ div.js
@@ -9,45 +9,48 @@ ColorList = ["#F38630", "#69D2E7", "#E0E4CC", "#FA6900", "#A7DBD8"];
 
 ////////////////// Adding Nodes to Array //////////////////
 
-Contents = {};
+///// DB가 없으므로 전역변수 .... 헣헣 /////
+Contents = {'NodeCnt':0};
 Contents['Node0'] = {'Content':'Home', 'Depth':0, 'Parent':null, 'Child':[]};
 
-function AddChildNode(Parent, Content) {
+function AddChildNode(NodeObj, Parent, Content) {
 	// 나중에 error 처리를 위해 (예외처리에 관한)if 문을 따로 둠. 일단은 console 창에서 찍는 걸로.
 	if (!Parent) { console.log("AddChildNode : No Parent Input"); return; }
-	if (typeof Contents[Parent] === "undefined") { console.log("AddChildNode : Parent undefined"); return; }
-	Name = Parent+'-'+Contents[Parent].Child.length;
+	if (typeof NodeObj[Parent] === "undefined") { console.log("AddChildNode : Parent undefined"); return; }
+	var Name = Parent+'-'+NodeObj[Parent].Child.length;
 
 	var newNode = {};
 	newNode.Content = (Content) ? Content : Name;
-	newNode.Depth = Contents[Parent].Depth + 1;
+	newNode.Depth = NodeObj[Parent].Depth + 1;
 	newNode.Parent = Parent;
 	newNode.Child = [];
 
 	console.log("Pushing "+Name+" to "+Parent);
-	Contents[Parent].Child.push(Name);
+	NodeObj[Parent].Child.push(Name);
 
-	Contents[Name] = newNode;
+	NodeObj[Name] = newNode;
+	NodeObj['NodeCnt']++;
 }
 
 // test 용으로 hard coding.
-AddChildNode('Node0', "hweofhqwobvncowabndasfwergvqergvq3wrvfvco;wqbnao;vcbnwobnvco;qwbncovj;weq");
-AddChildNode('Node0', "qhwoefbqwo;bvo;qwabsvcj;asndv;nql;nqo;w");
-AddChildNode('Node0');
-AddChildNode('Node0-1');
-AddChildNode('Node0-1', "adofchqwonvcoqw;nvo;qwnfqc[owinefoqw;c");
-AddChildNode('Node0-1', "");
-AddChildNode('Node0-2');
-AddChildNode('Node0-2');
-AddChildNode('Node0-2');
-AddChildNode('Node0-2');
-AddChildNode('Node0-2');
-AddChildNode('Node0-2-1');
-AddChildNode('Node0-2-1');
-AddChildNode('Node0-2-1');
-AddChildNode('Node0-2-1');
-AddChildNode('Node0-2-1');
-AddChildNode('Node0-2-1');
+// file IO 를 배워서, 현재 파일이 존재하는 서버에 로그인정보를 받고, 아이디명.txt 파일에 xml 식으로 저장하면..... 되지 않을 까.........
+AddChildNode(Contents, 'Node0', "hweofhqwobvncowabndasfwergvqergvq3wrvfvco;wqbnao;vcbnwobnvco;qwbncovj;weq");
+AddChildNode(Contents, 'Node0', "qhwoefbqwo;bvo;qwabsvcj;asndv;nql;nqo;w");
+AddChildNode(Contents, 'Node0');
+AddChildNode(Contents, 'Node0-1');
+AddChildNode(Contents, 'Node0-1', "adofchqwonvcoqw;nvo;qwnfqc[owinefoqw;c");
+AddChildNode(Contents, 'Node0-1', "");
+AddChildNode(Contents, 'Node0-2');
+AddChildNode(Contents, 'Node0-2');
+AddChildNode(Contents, 'Node0-2');
+AddChildNode(Contents, 'Node0-2');
+AddChildNode(Contents, 'Node0-2');
+AddChildNode(Contents, 'Node0-2-1');
+AddChildNode(Contents, 'Node0-2-1');
+AddChildNode(Contents, 'Node0-2-1');
+AddChildNode(Contents, 'Node0-2-1');
+AddChildNode(Contents, 'Node0-2-1');
+AddChildNode(Contents, 'Node0-2-1');
 
 console.log(Contents);
 
@@ -55,9 +58,48 @@ console.log(Contents);
 
 ////////////////// Positioning Nodes to HTML //////////////////
 
+function ChangeMain(NodeObj, Name) {
+	if (!Name) { console.log("ChangeMain : No Name Input"); return; }
+	if (typeof NodeObj[Name] === "undefined") { console.log("ChangeMain : Name undefined"); return; }
 
-///// 원으로 배치하려고 했으나..... 뭔가 반지름이 조금씩 안 맞음 ㅜㅜ /////
+	console.log("Creating Main");
+	var MainElement = GetNewNodeElement(NodeObj, Name, 'Main', ColorList);
+	document.getElementById('wrap').appendChild(MainElement);
 
+	var aChildElement = [];
+	for (var i = 0; i < NodeObj[Name].Child.length; i++) {
+		console.log("Creating Child "+i);
+		aChildElement[i] = GetNewNodeElement(NodeObj, NodeObj[Name].Child[i], 'Child', ColorList);
+		document.getElementById('wrap').appendChild(aChildElement[i]);
+	}
+
+	if (NodeObj[Name].Parent) {
+		console.log(aChildElement.length);
+		aChildElement.unshift(GetNewNodeElement(NodeObj, NodeObj[Name].Parent, 'Parent', ColorList));
+		document.getElementById('wrap').appendChild(aChildElement[0]);
+		console.log(aChildElement.length);
+	}
+
+	var LongerWidth = (document.getElementsByClassName('Main')[0].offsetWidth > document.getElementsByClassName('Main')[0].offsetHeight) ? document.getElementsByClassName('Main')[0].offsetWidth : document.getElementsByClassName('Main')[0].offsetHeight;
+	MakeCircle(aChildElement, LongerWidth*0.5);
+	
+}
+
+function GetNewNodeElement(NodeObj, Name, ClassType, ColorArray) {
+	var Element = document.createElement('div');
+	Element.id = Name;
+	Element.className = 'Node ';
+	Element.className += ClassType+' ';
+	Element.innerHTML = NodeObj[Name].Content;
+	if (ColorArray) Element.style.backgroundColor = ColorArray[NodeObj[Name].Depth % ColorArray.length];
+	console.log(Element);
+	// appendChild 도 공통된 행위라 GetNewNodeElement 에 묶을까 했으나, 받아서 직접 넣는 게 사용자 입장에서 맞는 것 같아서 일부러 뺌. 마치 new 로 받아오면 AddChild 는 직접 해 줘야 하듯이 헣헣
+	// document.getElementById('wrap').appendChild(Element);
+	return Element;
+}
+
+// referenced : http://hugogiraudel.com/2013/04/02/items-on-circle/
+///// 개선 필요 .. 반지름이 미묘하게 안 맞음 ㅜㅜ 노드가 여러개일 땐 별로 티가 안 나는데, 3개 정도일 때 치명적 ㅜㅜ /////
 function MakeCircle(ElementArray, ParentRadius, MarginRadius) {
 	console.log("Circle Start");
 	for (var i=0; i<ElementArray.length; i++) {
@@ -69,10 +111,10 @@ function MakeCircle(ElementArray, ParentRadius, MarginRadius) {
 		if (MarginRadius||MarginRadius===0) {
 			Radius += MarginRadius;
 		} else {
+			// 가로, 세로 중 더 긴 반지름 채택.
 			Radius += ( ElementArray[i].offsetWidth > ElementArray[i].offsetHeight ) ? ElementArray[i].offsetWidth*0.5 : ElementArray[i].offsetHeight*0.5;
 		}
 		
-		// var Radius = 150;
 		console.log("Circle "+i);
 		console.log("Degree : "+Degree);
 		console.log("Radius : "+Radius);
@@ -84,46 +126,3 @@ function MakeCircle(ElementArray, ParentRadius, MarginRadius) {
 		// ElementArray[i].style.webkitTransform = "rotate("+Degree+"deg) translate("+Radius+"px) rotate(-"+Degree+"deg)";
 	}
 }
-
-
-function ChangeMain(Name) {
-	if (!Name) { console.log("ChangeMain : No Name Input"); return; }
-	if (typeof Contents[Name] === "undefined") { console.log("ChangeMain : Name undefined"); return; }
-
-	function NewNodeElement(Name, Type) {
-		var Element = document.createElement('div');
-		Element.id = Name;
-		Element.className = 'Node ';
-		Element.className += Type+' ';
-		Element.innerHTML = Contents[Name].Content;
-		Element.style.backgroundColor = ColorList[Contents[Name].Depth % 5];
-		console.log(Element);
-		// appendChild 도 공통된 행위라 NewNodeElement 에 묶을까 했으나, 받아서 직접 넣는 게 사용자 입장에서 맞는 것 같아서 일부러 뺌. 마치 new 로 받아오면 AddChild 는 직접 해 줘야 하듯이 헣헣
-		// document.getElementById('wrap').appendChild(Element);
-		return Element;
-	}
-
-	console.log("Creating Main");
-	var MainElement = NewNodeElement(Name, 'Main');
-	document.getElementById('wrap').appendChild(MainElement);
-
-	var aChildElement = [];
-	for (var i = 0; i < Contents[Name].Child.length; i++) {
-		console.log("Creating Child "+i);
-		aChildElement[i] = NewNodeElement(Contents[Name].Child[i], 'Child');
-		document.getElementById('wrap').appendChild(aChildElement[i]);
-	}
-
-	if (Contents[Name].Parent) {
-		console.log(aChildElement.length);
-		aChildElement.unshift(NewNodeElement(Contents[Name].Parent, 'Parent'));
-		document.getElementById('wrap').appendChild(aChildElement[0]);
-		console.log(aChildElement.length);
-	}
-
-	var LongerWidth = (document.getElementsByClassName('Main')[0].offsetWidth > document.getElementsByClassName('Main')[0].offsetHeight) ? document.getElementsByClassName('Main')[0].offsetWidth : document.getElementsByClassName('Main')[0].offsetHeight;
-	MakeCircle(aChildElement, LongerWidth*0.5);
-	
-}
-
-
